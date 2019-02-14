d8653f03bde997d66d8a411da88eb1b3335ac0c9
Sampsa Vierros <sampsa.vierros@gmail.com>
2019-02-14 21:59:43 +0200

    Fixed copyright year.

--- ti.py
+++ ti.py
@@ -15,7 +15,7 @@
 #  along with this program; if not, write to the Free Software Foundation,
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
-#  Copyrigth (C) 1014 Sampsa Vierros
+#  Copyrigth (C) 2014 Sampsa Vierros
 #  All rights reserved.
 
 import serial
